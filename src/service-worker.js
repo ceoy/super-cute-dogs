ed899ee92d8ab513f5696f5b20bbed9f121e83ff
Tim Jahn <ceoymw@gmail.com>
2019-02-25 10:55:21 +0100

    service worker stuff

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,12 +1,27 @@
 var cacheName = 'supercutedogs-cache'
-var filesToCache = []
+self.__precacheManifest = [].concat(self.__precacheManifest || []).map((thing) => {
+    return thing.url
+});
 
 self.addEventListener('install', function(e) {
     console.log("[ServiceWorker] Install")
     e.waitUntil(
         caches.open(cacheName).then(function(cache) {
             console.log("[ServiceWorker] Caching App Shell")
-            return cache.addAll(filesToCache)
+            return cache.addAll(self.__precacheManifest)
+        })
+    )
+})
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            if (response) {
+                console.log("[ServiceWorker] Cache hit - return response")
+                return response
+            }
+            console.log("[ServiceWorker] Fetch Resource")
+            return fetch(event.request)
         })
     )
 })
